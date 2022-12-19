1f79d602b45e695d57e86b1207c5a8bff0b6c562
m00tY <ZacRex@outlook.com>
2022-12-19 15:59:29 -0500

    temp banner

--- src/App.js
+++ src/App.js
@@ -5,20 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <header className="center">
-          Zac Rex
-        </header>
-        <p className="subcenter">
-          Professional Dumb Dumb
-        </p>
-        
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        
-        <p class = "loading">
-          Website Coming Soon 
-        </p>
-        
+      Renovation in Progress...
       </header>
     </div>
   );
